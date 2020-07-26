7c87f429623b95b8c573236c8c32608d34a0ad17
Kanchuking1 <kanchuking1@gmail.com>
2020-07-26 07:53:22 +0530

    Final commit with EchoAR files

---
+++ sigmaHack/Assets/Scripts/SceneChangeScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChangeScript : MonoBehaviour
+{
+    public ScreenOrientation screenOrientation = ScreenOrientation.Portrait;
+
+    private void Start()
+    {
+        Screen.orientation = screenOrientation;
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void GoToDIYMenu()
+    {
+        SceneManager.LoadScene("DIYMenuScene");
+    }
+
+    public void GoToNavMenu()
+    {
+        SceneManager.LoadScene("DIYNavMenu");
+    }
+
+    public void GoToDIYShelf()
+    {
+        SceneManager.LoadScene("DIYTutShelf");
+    }
+
+    public void GoToDIYAC()
+    {
+        SceneManager.LoadScene("DIYTutAC");
+    }
+
+    public void GoToLeaderBoard()
+    {
+        SceneManager.LoadScene("LeaderBoardScene");
+    }
+
+    public void GoToWebNav()
+    {
+        SceneManager.LoadScene("WebsiteTutScene");
+    }
+    
+}
